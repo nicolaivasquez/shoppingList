833c264d0bf11fea6286d722355409e03250ca46
Nicolai Vasquez <nrvasquez@gmail.com>
2015-10-12 22:48:29 +0800

    initial commit using yeoman gulp-angular generator

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('shopList')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
