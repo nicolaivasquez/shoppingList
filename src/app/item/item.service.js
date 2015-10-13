febf396c08f9955775b39269853a833f7396341b
Nicolai Vasquez <nrvasquez@gmail.com>
2015-10-13 21:20:28 +0800

    item service unit tests

--- src/app/item/item.service.js
+++ src/app/item/item.service.js
@@ -5,9 +5,9 @@
         .module('shopList')
         .factory('ItemService', ItemService);
 
-    function ItemService($http) {
+    function ItemService($http, $log) {
 
-        var api = 'http://shoppingList.app/lists';
+        var api = 'http://shoppinglist.app/lists';
 
         var service = {
             getItems: getItems
@@ -16,11 +16,17 @@
         return service;
 
         function getItems(listSlug) {
-            return $http.get(api + '/' + listSlug + '/items').then(processItems);
+            return $http.get(api + '/' + listSlug + '/items')
+                .then(processItems)
+                .catch(processError, 'hello');
         }
 
         function processItems(response) {
             return response.data.items;
+        }
+
+        function processError(error) {
+            $log.error('XHR failed to get items.\n' + angular.toJson(error.data, true));
         }
 
     }
